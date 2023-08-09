385a97bdfaa35f01cf15bd922b0e2c4aad5db487
Kyle A Pearson <kyle.a.pearson@jpl.nasa.gov>
2023-08-09 13:38:40 -0700

    evaluation scripts

---
+++ eval_classifier.py
@@ -0,0 +1,108 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import time
+import os
+import cv2
+import glob
+import glymur
+import pickle
+import argparse
+import numpy as np
+import tensorflow as tf
+from skimage.util.shape import view_as_windows
+from scipy.ndimage import binary_erosion, binary_dilation, binary_closing, label
+
+from train_classifier import make_rnet, make_mnet, make_cnet, blockDCT
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-w", '--weights', help='which pretrained weights to use for the network', type=str,
+                         #default="models/cnn_256_spatial_2_2.h5")
+                         #default="models/mobilenet_128_spatial_2_4.h5")
+                         default="models/mobilenet_256_spatial_2_1.h5")
+                         #default="models/cnn_256_dct_2_4.h5")
+
+    parser.add_argument("-i", "--input", help="HiRISE Image Filepath", type=str,
+                         #default="training/data/brain_coral/PSP_001410_2210/PSP_001410_2210_RED.JP2")
+                         default="images/ESP_016287_2205_RED.JP2")
+                         #default="images/ESP_016215_2190_RED.JP2")
+                         #default="images/ESP_060698_2220_RED.JP2")
+                         #default="images/ESP_077488_2205_RED.JP2")
+
+    parser.add_argument('--threads', action='store', default=8, type=int, 
+                        help='Number of threads to use when decoding JPEG2000 files')
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+
+    # parse command line arguments
+    args = parse_args()
+
+    # set number of threads for glymur to decode with
+    glymur.set_option('lib.num_threads', args.threads)
+
+    # parse model params from name in weights files (eg. cnn_128_spatial_2_2.h5)
+    parts = os.path.basename(args.weights).split("_")
+    model_type = parts[0]                    # cnn, mobilenet, resnet
+    size = int(parts[1])                     # tiling size
+    stride = int(parts[1])
+    dtype = parts[2]                         # spatial or dct
+    res = int(parts[3].split(".")[0])        # resolution factor for glymur
+
+    # make model
+    if model_type == "cnn":
+        model_fn = make_cnet
+    elif model_type == "mobilenet":
+        model_fn = make_mnet
+    elif model_type == "resnet":
+        model_fn = make_rnet
+
+    if dtype == 'spatial':
+        input_size = (size, size, 1)
+    elif dtype == 'dct':
+        input_size = (size//8, size//8, 64)
+    
+    # load weights
+    model = model_fn(input_size, 2)
+    model.summary()
+    model.load_weights(args.weights)
+    print("weights loaded!")
+
+    # check if input exists
+    if not os.path.exists(args.input):
+        print("input file does not exist")
+
+    idata = glymur.Jp2k(args.input).read(rlevel=res).astype(np.float32)
+
+    sfactor = 1#0.9 # stride factor, how much to overlap tiles (1-x)
+
+    BI = view_as_windows(idata, size, step=int(stride*sfactor))
+    print(" tiles in map:",BI.shape)
+    
+    X = BI.reshape(-1, size, size)
+
+    if dtype == 'dct':
+        X = blockDCT(X)
+
+    pred = model.predict(X, batch_size=16, verbose=1)
+
+    # reshape back (BI.shape[0], BI.shape[1], classes)
+    BO = pred.reshape(BI.shape[0], BI.shape[1], 2)
+
+    # create mask using max of channel
+    mask = BO.argmax(axis=2) == 0 # 0th channel is brain coral
+
+    # clean mask by removing single pixels not surrounded by other pixels, erosion is too aggressive
+    groups, ngroups = label(mask, structure=np.ones((3,3)))
+
+    # remove groups smaller than 1 pixel
+    for i in range(1, ngroups+1):
+        if (groups == i).sum() < 2:
+            mask[groups == i] = False
+
+    # save mask using cv2
+    maskfile = os.path.splitext(args.input)[0] + "_classifier_mask.png"
+    print("saving mask to", maskfile)
+    cv2.imwrite(maskfile, mask.astype(np.uint8)*255)
---
+++ eval_segmentation.py
@@ -0,0 +1,151 @@
+import os
+import gc
+import cv2
+import glymur
+import argparse
+import numpy as np
+from tqdm import tqdm
+import tensorflow as tf
+from skimage.util.shape import view_as_windows
+from scipy.ndimage import binary_erosion, binary_dilation, label
+
+from train_unet import make_unet3
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-w", '--weights', help='which pretrained weights to use for the network', type=str,
+                        #default="models/unet_512_0_rescale_ft_weights.h5")
+                        default="models/unet_512_0_normalize_ft_weights.h5")
+
+
+    parser.add_argument("-i", "--input", help="HiRISE Image Filepath", type=str,
+                         #default="images/PSP_001410_2210_RED_A_01_ORTHO.JP2")
+                         #default="images/ESP_016287_2205_RED_A_01_ORTHO.JP2")
+                         default="images/ESP_037262_1845_RED_A_01_ORTHO.JP2")
+                         #default="images/ESP_077488_2205_RED.JP2")
+
+    parser.add_argument('--threads', action='store', default=8, type=int, 
+                        help='Number of threads to use when decoding JPEG2000 files')
+
+    parser.add_argument('--fast', action='store_true', help='skips robust clean up of mask (removes single pixels not surrounded by other pixels)')
+
+    parser.add_argument("--gpu", default=0, type=int, help='specify which gpu to use')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    # parse command line arguments
+    args = parse_args()
+
+    # set number of threads for glymur to decode with
+    glymur.set_option('lib.num_threads', args.threads)
+
+    # Restrict TensorFlow to use a particular GPU, if one is available
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            if args.gpu > len(gpus):
+                raise(f"gpu number {args.gpu_num} not supported")
+            tf.config.set_visible_devices(gpus[args.gpu], 'GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
+
+    # convert model name to size and res parameters
+    # e.g. unet_512_0_rescale_ft_weights.h5
+    parts = args.weights.split("_")
+    args.size = int(parts[1])
+    args.res = int(parts[2])
+    preprocessing = parts[3]
+
+    # make model
+    unet, encoder = make_unet3(input_shape=(args.size,args.size), output_channels=2, preprocessing=preprocessing)
+
+    # load weights
+    if os.path.exists(args.weights):
+        if 'encoder' in args.weights:
+            encoder.load_weights(args.weights)
+            segmentor = encoder
+        else:
+            unet.load_weights(args.weights)
+            segmentor = unet
+
+        segmentor.summary()
+        print(f"Loaded weights from {args.weights}")
+    else:
+        raise(f"Could not find weights file {args.weights}")
+
+    # load image
+    print(f"Loading image {args.input}...")
+    idata = glymur.Jp2k(args.input).read(rlevel=args.res)
+
+    # tile image, reduces boxy artifacts
+    overlap = 0.5
+    BI = view_as_windows(idata, args.size, step=int(args.size*overlap))
+    
+    # mask out black parts of image
+    imask = np.mean(BI, axis=(2,3))>10
+
+    # flatten array
+    BIR = BI[imask].reshape(-1, args.size, args.size)
+
+    # batch data cus gpu memory is limited
+    pred = np.zeros((BIR.shape[0], *segmentor.output.shape[1:]))
+
+    # batch size depends on computer memory
+    if args.size > 512:
+        batch = 500
+    else:
+        batch = 1000
+
+    # print number of batches
+    print(f"Segmenting {BIR.shape[0]} tiles in {BIR.shape[0]//batch+1} batches of {batch} tiles")
+
+    # batch data based on gpu memory
+    for i in range(0,BIR.shape[0],batch):
+        sub=slice(i,i+batch)
+        pred[sub] = segmentor.predict(BIR[sub], batch_size=8, verbose=1)
+
+        # clean up memory for really large image, really slow :(
+        if BIR.shape[0] > batch and i%batch == 0:
+            _ = gc.collect()
+
+    print('Reshaping output...')
+    # only take center of each prediction, this ignores edge effects in the cnn
+    crop = slice(int(segmentor.output.shape[1]*(0.5-overlap/2)),int(segmentor.output.shape[1]*(0.5+overlap/2)))
+    pred = pred[:, crop,crop]
+
+    # reshape to original image size
+    BO = np.zeros((BI.shape[0], BI.shape[1], pred[0].shape[0], pred[0].shape[1], pred[0].shape[2]))
+    BO[imask] = pred
+
+    # convert to image with multiple channels
+    heatmap = BO.swapaxes(1,2).reshape(BI.shape[0]*pred[0].shape[0], BI.shape[1]*pred[0].shape[1], pred[0].shape[2] )
+
+    # create mask using max of channel
+    mask = heatmap[:,:,0] > 0.55 # 0th channel is brain coral
+
+    # clean up mask
+    print("Cleaning up mask...")
+    mask = binary_erosion(mask, iterations=4)
+
+    # clean mask by removing single pixels not surrounded by other pixels, erosion is too aggressive
+    groups, ngroups = label(mask, structure=np.ones((3,3)))
+
+    if not args.fast:
+        # remove groups smaller than 3 pixel, may take ~1-2 hours
+        for i in tqdm(range(1, ngroups+1)):
+            if (groups == i).sum() < 4:
+                mask[groups == i] = False
+
+    mask = binary_dilation(mask, iterations=4)
+
+    # save mask to disk
+    maskfile = os.path.splitext(args.input)[0] + "_segmentation_mask.png"
+    cv2.imwrite(maskfile, mask.astype(np.uint8)*255)
+    print("Saved mask to", maskfile)
